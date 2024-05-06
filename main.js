fa241920905b2c653d803c4a58c8e2ca9b6b83c8
4uwebsite <ramee.nana@gmail.com>
2024-05-06 21:22:59 +0530

    done

---
+++ main.js
@@ -0,0 +1,34 @@
+let div = document.getElementById('info')
+let h2 = document.getElementById('on-off-line')
+
+const conn = navigator.connection
+
+function updateUI(evt){
+    console.log('change', evt)
+
+    h2.innerText = navigator.onLine
+    
+    const downlink = conn.downlink
+    const downlinkMax = conn.downlinkMax
+    const effectiveType = conn.effectiveType
+    const rtt = conn.rtt
+    const saveData = conn.saveData
+    const type = conn.type
+    
+    const text = `
+    downlink: ${downlink}Mbps
+    downlinkMax: ${downlinkMax}Mbps 🧪
+    effectiveType: ${effectiveType}
+    rtt: ${rtt}ms
+    saveData: ${saveData}
+    type: ${type} 🧪
+    `
+    
+    div.innerText = text
+}
+
+conn.addEventListener('change', evt => {
+    updateUI(evt)
+})
+
+updateUI(null)
